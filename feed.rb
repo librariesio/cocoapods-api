031621e79669558512d31b5b283d8ac0886caefd
Jeremy Katz <jeremy@tidelift.com>
2018-07-25 11:58:48 -0400

    Fix feedtosis to listen to redis_server environment variable too

--- feed.rb
+++ feed.rb
@@ -3,9 +3,13 @@ Bundler.require
 
 require './cocoapods_repo'
 
-client = Feedtosis::Client.new('http://github.com/CocoaPods/Specs/commits/master.atom', backend: Moneta.new(:Redis))
+redis_uri = ENV['REDIS_SERVER'] || 'localhost:6379'
+client = Feedtosis::Client.new('http://github.com/CocoaPods/Specs/commits/master.atom',
+                               backend: Moneta.new(:Redis,
+                                                   host: URI.parse(redis_uri).host,
+                                                   port: URI.parse(redis_uri).port))
 while(true) do
   new_entries = client.fetch.new_entries
-  CocoapodsRepo.new(ENV['REDIS_SERVER']).update_pods if new_entries
+  CocoapodsRepo.new(redis_uri).update_pods if new_entries
   sleep 30
 end
