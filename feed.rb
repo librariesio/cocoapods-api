31eebde9e7138bcc860a37758ec204339d324e72
Jeremy Katz <jeremy@tidelift.com>
2018-07-25 09:48:31 -0400

    Listen to environment variable to find redis server

--- feed.rb
+++ feed.rb
@@ -6,6 +6,6 @@ require './cocoapods_repo'
 client = Feedtosis::Client.new('http://github.com/CocoaPods/Specs/commits/master.atom', backend: Moneta.new(:Redis))
 while(true) do
   new_entries = client.fetch.new_entries
-  CocoapodsRepo.new.update_pods if new_entries
+  CocoapodsRepo.new(ENV['REDIS_SERVER']).update_pods if new_entries
   sleep 30
 end
